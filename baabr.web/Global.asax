f58b45e43e8a15419e20438e0ee2ab9e17497102
Babar Ayub <ayubbabar59@gmail.com>
2019-04-02 07:22:36 -0700

    project

---
+++ baabr.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="baabr.web.MvcApplication" Language="C#" %>
---
+++ baabr.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(baabr.web.Startup))]
+namespace baabr.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
